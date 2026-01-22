ab67e7dd50b59277fde0fb3fb5123a0de0be0fb6
Md Maruf Hasan <160125834+MarufHasanCSE@users.noreply.github.com>
2026-01-22 13:57:18 +0600

    Add sentence generation with two stories

--- BuildSentenceMaker.js
+++ BuildSentenceMaker.js
@@ -1,1 +1,22 @@
 
+let adjective = "lazy";
+let noun = "Coder";
+let noun2 = "Chicken";
+let place = "BachelorHouse";
+let adjective2 = "normal";
+let verb = "breathing";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "great";
+noun = "Soul";
+verb = "walking";
+place = "Cave";
+adjective2 = "unusual";
+noun2 = "Chicken";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
