3f63a7651044fbdc6e13962980bec89f64558126
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-01-23 21:36:29 +0600

    added some segments

--- BuildStringInspector.js
+++ BuildStringInspector.js
@@ -3,6 +3,8 @@ const fccSentence = "freeCodeCamp is a great place to learn web development.";
 console.log("Here are some examples of the includes() method:");
 
 const hasFreeCodeCamp = fccSentence.includes("freeCodeCamp");
-
 console.log(`fccSentence.includes("freeCodeCamp") returns ${hasFreeCodeCamp} because the word "freeCodeCamp" is in the sentence.`);
 
+let hasJavaScript = fccSentence.includes("JavaScript");
+
+console.log(`fccSentence.includes("JavaScript") returns ${hasJavaScript} because the word "JavaScript" is not in the sentence.`);
