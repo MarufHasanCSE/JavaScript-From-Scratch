a372ed7c97f4a4c5606f12cae28310d6e5b01213
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-01-23 19:54:33 +0600

    Completed The Task From FREECODECAMP

--- buildTeacherChatBot.js
+++ buildTeacherChatBot.js
@@ -24,5 +24,20 @@ console.log(`Here is an example of accessing the first letter in the word ${subj
 console.log(subject[0]);
 
 console.log(`Here is an example of accessing the second letter in the word ${subject}.`);
-
 console.log(subject[1]);
+
+console.log(`Here is an example of accessing the last letter in the word ${subject}.`);
+
+const lastCharacter = subject[subject.length - 1];
+console.log(lastCharacter);
+
+const learningIsFunSentence = "Learning is fun.";
+
+console.log("Here are examples of finding the positions of substrings in the sentence.");
+
+console.log(learningIsFunSentence.indexOf("Learning"));
+
+console.log(learningIsFunSentence.indexOf("fun"));
+console.log(learningIsFunSentence.indexOf("learning"));
+
+console.log("I hope you enjoyed learning today.");
