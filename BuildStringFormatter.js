676d305620506dc9ca8b1c754de103a308a2c49c
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-01-23 22:39:50 +0600

    added some segments

--- BuildStringFormatter.js
+++ BuildStringFormatter.js
@@ -22,7 +22,10 @@ const lowerCaseInput = cleanedInput.toLowerCase();
 console.log("Result of using the toLowerCase() method:");
 console.log(lowerCaseInput);
 
-let lowercaseWord = "camelcase";
-let camelCasedVersion  = "";
+const lowercaseWord = "camelcase";
+let camelCasedVersion = lowercaseWord.slice(0, 5)
 console.log("Camel cased version:");
+console.log(camelCasedVersion);
+
+ camelCasedVersion = lowercaseWord.slice(0, 5) + lowercaseWord[5].toUpperCase();
 console.log(camelCasedVersion);
