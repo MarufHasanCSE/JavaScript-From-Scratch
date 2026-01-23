874121c733ba30150b309d4c78243e29fdc5ccc9
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-01-23 23:22:17 +0600

    added some segments

--- BuildStringTransformer.js
+++ BuildStringTransformer.js
@@ -3,6 +3,14 @@ console.log("Original string:");
 console.log(originalString);
 
 const replacedString = originalString.replace("cats", "dogs");
-
 console.log("After using the replace() method:");
 console.log(replacedString);
+
+const exampleSentence = "I love cats and cats are so much fun!";
+console.log("Original sentence:");
+console.log(exampleSentence);
+
+let dogsOnlySentence = exampleSentence.replace("cats", "dogs");
+
+console.log("Replacing all occurrences of cats with dogs:");
+console.log(dogsOnlySentence);
