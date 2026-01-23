29f44741c844aa966cebb8e710a485fd62430af9
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-01-23 23:29:18 +0600

    added some segments

--- BuildStringTransformer.js
+++ BuildStringTransformer.js
@@ -10,7 +10,16 @@ const exampleSentence = "I love cats and cats are so much fun!";
 console.log("Original sentence:");
 console.log(exampleSentence);
 
-let dogsOnlySentence = exampleSentence.replace("cats", "dogs");
-
+const dogsOnlySentence = exampleSentence.replaceAll("cats", "dogs");
 console.log("Replacing all occurrences of cats with dogs:");
 console.log(dogsOnlySentence);
+
+const learningSentence = "I love learning!";
+console.log("Original learning sentence:");
+console.log(learningSentence);
+
+const repeatedLove = "love ".repeat(3).trimEnd();
+console.log(repeatedLove);
+
+const newSentence = `I ${repeatedLove} learning.`;
+console.log(newSentence);
