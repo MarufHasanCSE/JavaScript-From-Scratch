7a49d7a33d21f4ac18f16527812ed630fc8c4b77
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-01-24 12:00:11 +0600

    Added some segments

--- BuildaLogicCheckerApp.js
+++ BuildaLogicCheckerApp.js
@@ -6,7 +6,13 @@ if (hasDeveloperJob) {
 
 const isTimmyAGamer = false;
 
-
-if (isTimmyAGamer ) {
+if (isTimmyAGamer) {
   console.log("Timmy loves to play World of Warcraft.");
 }
+
+const timmyAge = 18;
+
+if(timmyAge>=16){
+ console.log("Timmy is old enough to drive.");
+
+};
