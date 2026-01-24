f03ba57cffa89e5f7f0fa8636ca2679c3343dfeb
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-01-24 11:54:10 +0600

    Added some segments

--- BuildaLogicCheckerApp.js
+++ BuildaLogicCheckerApp.js
@@ -1,2 +1,12 @@
-const truthyOrFalsy = true;
-console.log(Boolean(truthyOrFalsy));
+const hasDeveloperJob = true;
+
+if (hasDeveloperJob) {
+  console.log("Timmy is employed as a developer.");
+}
+
+const isTimmyAGamer = false;
+
+
+if (isTimmyAGamer ) {
+  console.log("Timmy loves to play World of Warcraft.");
+}
