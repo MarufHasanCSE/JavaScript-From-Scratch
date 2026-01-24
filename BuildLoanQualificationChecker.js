863dc710b75ffc1e169b32e4ec4059b1d4cb48b7
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-01-24 18:17:23 +0600

    Completed!

--- BuildLoanQualificationChecker.js
+++ BuildLoanQualificationChecker.js
@@ -14,6 +14,17 @@ function getLoanMessage(annualIncome, creditScore) {
     return "You qualify for a condo and car loan."
   } else if (annualIncome >= minIncomeForCar && creditScore >= minCreditScoreForCar) {
     return "You qualify for a car loan."
+  } else {
+    return "You don't qualify for any loans."
   }
-
 }
+
+const duplexLoanMsg = getLoanMessage(85000, 850);
+const condoLoanMsg = getLoanMessage(65000, 690);
+const carLoanMsg = getLoanMessage(45000, 660);
+const noLoanMsg = getLoanMessage(25000, 550);
+
+console.log(duplexLoanMsg);
+console.log(condoLoanMsg);
+console.log(carLoanMsg);
+console.log(noLoanMsg);
