97da37122c03a22d17afef9f333fe70c34ff43e9
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-01-25 15:46:18 +0600

    Added Some Segments

--- BuildaShoppingList.js
+++ BuildaShoppingList.js
@@ -18,5 +18,13 @@ console.log(getShoppingListMsg(shoppingList));
 console.log("It looks like we need to get some cooking oil.");
 
 shoppingList.unshift("Vegetable Oil");
+console.log(getShoppingListMsg(shoppingList));
+
+shoppingList.push("Popcorn", "Beef Jerky", "Potato Chips");
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("This looks like too much junk food.");
+
+shoppingList.pop();
 
 console.log(getShoppingListMsg(shoppingList));
