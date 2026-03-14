d39e7b3da147be4cbbc5e3b75bfb144bb9899239
Md Maruf Hasan <160125834+MarufHasanCSE@users.noreply.github.com>
2026-03-14 20:21:27 +0600

    Update BuildCelsiusToFahrenheitConverter.js

--- BuildCelsiusToFahrenheitConverter.js
+++ BuildCelsiusToFahrenheitConverter.js
@@ -5,3 +5,5 @@ console.log(convertCtoF(0));
 console.log(convertCtoF(20));   
 console.log(convertCtoF(37));   
 console.log(convertCtoF(100));  
+console.log(convertCtoF(40));   
+console.log(convertCtoF(102));  
